1138b0197328dfdbe03cc0fd5cf4fc888b2cfa0b
Gregory Arnheiter <gmarnheiter@gmail.com>
2019-12-17 11:27:11 -0800

    Working clean, monior changes made to clean up code i.e. var to const etc

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const https = require('https');
 
-var timeSheetFormController = require('../source/controllers/timeSheetFormController');
-var timeSheetsController = require('../source/controllers/timeSheetsController');
-var foremenController = require('../source/controllers/foremenController');
-var createAccountController = require('../source/controllers/createAccountController');
-var loginController = require('../source/controllers/loginController');
+const timeSheetFormController = require('../source/controllers/timeSheetFormController');
+const timeSheetsController = require('../source/controllers/timeSheetsController');
+const foremenController = require('../source/controllers/foremenController');
+const createAccountController = require('../source/controllers/createAccountController');
+const loginController = require('../source/controllers/loginController');
 
 
 /* GET home page. */
@@ -18,9 +18,9 @@ router.get('/createAccount', function(req, res, next) {
   res.render('createAccount', { title : 'Create Your Account'});
 });
 
-router.post('/createAccount', async function(req, res, next){
+router.post('/createAccount', async function(req, res, next) {
   try {
-    let result = await createAccountController.createAccount(req.body);
+    const result = await createAccountController.createAccount(req.body);
   }
   catch(error) {
     throw error;
@@ -28,13 +28,13 @@ router.post('/createAccount', async function(req, res, next){
     res.redirect('/');
 });
 
-router.get('/login', function(req, res, next){
+router.get('/login', function(req, res, next) {
   res.render('login', {title : 'Login Page'});
 });
 
-router.post('/login', async function(req, res, next){
+router.post('/login', async function(req, res, next) {
   try {
-    let result = await loginController.login(req.body);
+    const result = await loginController.login(req.body);
   }
   catch(error) {
     throw error;
@@ -43,12 +43,12 @@ router.post('/login', async function(req, res, next){
   res.redirect('/');
 });
 
-router.get('/timeSheetForm', async function(req, res, next){
-  let foremenArray = await foremenController.returnForemen();
+router.get('/timeSheetForm', async function(req, res, next) {
+  const foremenArray = await foremenController.returnForemen();
   res.render('timeSheetForm', {title : 'Foreman HOURS Form', foremenArray : foremenArray});
 });
 
-router.post('/timeSheetForm', async function(req, res, next){
+router.post('/timeSheetForm', async function(req, res, next) {
   try {
     console.log(JSON.stringify(req.body, null, 2));
     let result = await timeSheetFormController.insertTime(req.body);
@@ -60,24 +60,29 @@ router.post('/timeSheetForm', async function(req, res, next){
     res.redirect('/');
 });
 
-router.get('/foremen', async function(req, res, next){
-  let foremenArray = await foremenController.returnForemen();
+router.get('/foremen', async function(req, res, next) {
+  const foremenArray = await foremenController.returnForemen();
   res.render('foremen', {title : 'Foreman List', foremenArray : foremenArray});
 });
 
-router.get('/timeSheets', async function(req, res, next){
-  let timeSheetsArray = await timeSheetsController.returnTimeSheets();
+router.get('/timeSheets', async function(req, res, next) {
+  const timeSheetsArray = await timeSheetsController.returnTimeSheets();
   res.render('timeSheets', {title : 'Time Sheets List', timeSheetsArray : timeSheetsArray});
 });
 
-router.get('/timeSheets/:timeSheetsID', async function (req, res) {
+router.get('/timeSheets/delete/:timeSheetsID', async function (req, res) {
   const timesheetID = req.params.timeSheetsID;
   await timeSheetsController.deleteTimeSheet(timesheetID);
   res.redirect('/timeSheets');
 });
 
-router.get('/Jobs', function(req, res, next){
-  res.render('jobs', {title : 'Jobs'});
+// router.get('/timeSheets/edit/:timeSheetsID', async function(req, res) {
+//   const timesheetID = req.params.timeSheetsID;
+// });
+
+router.get('/Jobs', async function(req, res, next) {
+  const jobsArray = await jobsController.returnJobs();
+  res.render('jobs', {title : 'Jobs', jobsArray : jobsArray});
 });
 
 router.get('/api/foremen', async (req, res, next) => {
